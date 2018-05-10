619b1f7a37ae4f278e0b853446428cc59b86b7a8
pinyin <bob432091@gmail.com>
2018-05-10 09:45:38 +0800

    0.0.2 empty reducer should return original store when original store is defined

--- src/Reducers.ts
+++ src/Reducers.ts
@@ -1,5 +1,5 @@
 import {Reducer} from 'redux'
-import {FluxStandardAction} from './FluxStandardAction'
+import {FluxStandardAction} from './Action'
 
 export type Reducers<Store extends object, Actions extends object, ActionTypes extends keyof Actions = keyof Actions> = {
     [Key in ActionTypes]: Reducer<Store, FluxStandardAction<Key, Actions[Key]>>
--- src/getEmptyReducer.ts
+++ src/getEmptyReducer.ts
@@ -1,9 +1,8 @@
 import {Reducer} from 'redux'
 import {Action} from './Action'
-import {FluxStandardAction} from './FluxStandardAction'
 
 export function getEmptyReducer<Store extends object, Actions extends object>(
     defaultStore: Store
 ): Reducer<Store, Action<Actions>> {
-    return () => defaultStore
+    return (store: Store | undefined) => store || defaultStore
 }
