a926066d51e34c6ffcbe4941ecf8433f8d881d67
pinyin <bob432091@gmail.com>
2018-07-09 23:12:24 +0800

    better Action type

--- src/Action.ts
+++ src/Action.ts
@@ -1,5 +1,5 @@
-import {ExtractByTag, TagFromMap, TaggedByMap, TagMap} from '@pinyin/types'
+import {TagFromMap, TaggedByMap, TagMap} from '@pinyin/types'
 
 export type Action<A extends TagMap = TagMap, T extends TagFromMap<A> = TagFromMap<A>> =
-    ExtractByTag<TaggedByMap<A>, T>
+    TaggedByMap<A, T>
 
